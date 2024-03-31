1851b1b854caf087d951881365f5fa6036f2d068
sumitsingh00 <sumit00134@gmail.com>
2024-04-01 04:41:18 +0530

    lastCommit

--- controllers/deleteTodo.js
+++ controllers/deleteTodo.js
@@ -5,8 +5,10 @@ const Todo = require("../models/Todo");
 
 exports.deleteTodo = async (req, res) => {
     try {
-        const {id} = req.params;
-        await Todo.findByIdAndDelete({id});
+        const id = req.params.id;
+        // jiski bhi _id == id  usko le aao
+        // always rembember isko { _id: id } ism likhna hai
+        await Todo.findByIdAndDelete({ _id: id });
 
         res.json({
             success: true,
