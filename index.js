a1e77116f691f35dab78d7fcabdb2b9520556f3c
sumitsingh00 <sumit00134@gmail.com>
2024-04-01 03:47:38 +0530

    last commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//middleware to parse json request body
+app.use(express.json());
+
+//OR THIS
+// const bodyParser = require("body-parser");
+// // parse JSON data & add it to the request.Body object
+// app.use(bodyParser.json());
+
+// The app. use() function is used to mount the specified middleware function 
+
+
+//import routes for TODO API
+const todoRoutes = require("./routes/todos");
+
+//mount the todo API routes
+app.use("/api/v1", todoRoutes);
+
+//start server
+app.listen(PORT, () => {
+  console.log(`Server started successfully at ${PORT}`);
+});
+
+//connect to the database.// inport database then mount it
+const dbConnect = require("./config/database");
+dbConnect();
+
+//default Route
+app.get("/", (req, res) => {
+  res.send(`<h1> This is HOMEPAGE baby</h1>`);
+});
