502b6ce71f2fd9845b69163a25a69cb0f4ee1339
sumitsingh00 <sumit00134@gmail.com>
2024-05-13 10:38:21 +0530

    LastCommit

--- routes/todos.js
+++ routes/todos.js
@@ -7,7 +7,7 @@ const { getTodos,getTodoById } = require("../controllers/getTodo.js");
 const { updateTodo} = require("../controllers/updateTodo");
 const { deleteTodo} = require("../controllers/deleteTodo");
 
-//define Api routes
+//define Api routes for all controllers
 router.post("/createTodo", createTodo);
 router.get("/getTodos", getTodos);
 router.get("/getTodo/:id", getTodoById);
